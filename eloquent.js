5a439c763820f8db4e399400721ec70469e8975b
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-05-28 16:27:39 +1200

    add countBs function and index page to debug

--- eloquent.js
+++ eloquent.js
@@ -5,3 +5,30 @@ function min(a, b){
     };
 console.log(min(0, 10));
 console.log(min(0, -10));
+
+
+
+// Counting number of 'B' occurence in string
+function countBs(str) {
+    let count = 0;
+      for(i=0; i<str.length; i++) {
+        if(str[i] == 'B') {
+          count ++;
+        }
+        else {
+        };
+    };
+    return count;
+};
+
+// Counting number of chosen occurence in string
+
+function countChar (string, a){
+    let count = 0;
+    
+};   
+      
+    console.log(countBs("BBC"));
+    // → 
+    console.log(countChar("kakkerlak", "k"));
+    // →4
