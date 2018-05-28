9a2c27a5c5c9d05954841a40479dc7f4125b63a1
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-05-28 17:41:58 +1200

    add countChar function

--- eloquent.js
+++ eloquent.js
@@ -11,21 +11,29 @@ console.log(min(0, -10));
 // Counting number of 'B' occurence in string
 function countBs(str) {
     let count = 0;
-      for(i=0; i<str.length; i++) {
-        if(str[i] == 'B') {
+    for(i=0; i<str.length; i++) {
+    if(str[i] == 'B') {
+        count ++;
+    }
+    else {
+    };
+    };
+    return count;
+};
+
+// Counting number of chosen occurence in string
+function countChar (string, a){
+    let count = 0;
+  	let aLength = a.length;
+    for(i=0; i<string.length; i++) {
+    	var stringCuts = string.slice(i, i + aLength);
+        if(stringCuts == a) {
           count ++;
         }
         else {
         };
     };
     return count;
-};
-
-// Counting number of chosen occurence in string
-
-function countChar (string, a){
-    let count = 0;
-    
 };   
       
     console.log(countBs("BBC"));
